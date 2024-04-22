342374f8726211c197e82fefff72b1d91c32b916
Myriad-Raki-Diallo <raki.diallo@myriadgroup.com>
2024-04-22 22:15:59 +0000

    xxxyyy

---
+++ EhodVenteEnLigne/Models/Services/LanguageService.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using System;
+
+namespace EhodBoutiqueEnLigne.Models.Services
+{
+    public class LanguageService : ILanguageService
+    {
+        /// <summary>
+        /// Set the UI language
+        /// </summary>
+        public void ChangeUiLanguage(HttpContext context, string language)
+        {
+            string culture = SetCulture(language);
+            UpdateCultureCookie(context, culture);
+        }
+
+        /// <summary>
+        /// Set the culture
+        /// </summary>
+        public string SetCulture(string language)
+        {
+            string culture;
+            switch (language.ToLowerInvariant()) // Convertir en minuscules pour éviter les problèmes de casse
+            {
+                case "english":
+                    culture = "en";
+                    break;
+                case "french":
+                    culture = "fr";
+                    break;
+                case "wolof":
+                    culture = "wo";
+                    break;
+                default:
+                    culture = "en";
+                    break;
+            }
+
+            return culture;
+        }
+
+        /// <summary>
+        /// Update the culture cookie
+        /// </summary>
+        public void UpdateCultureCookie(HttpContext context, string culture)
+        {
+            // Configurer le cookie pour expirer dans un an
+            context.Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) });
+        }
+    }
+}
